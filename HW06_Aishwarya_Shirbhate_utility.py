32f12b688122a31906cf5a6d749bd9f7edcfc1c7
Aishwarya Shirbhate <92288147+aishwaryashirbhate@users.noreply.github.com>
2022-02-28 18:04:35 -0500

    HW06 utility added
    added gameplay log file which logs game stats and 5 letter word list

---
+++ HW06_Aishwarya_Shirbhate_utility.py
@@ -0,0 +1,32 @@
+# Homework Assignment 06
+# from Aishwarya Shirbhate 20005242
+# Pseudocode #
+# define five_letter_word() function
+# {
+# open word_list.txt file to read
+# store all the words with length 5 in words list
+# close file
+# open 5letterwords.txt file to write
+# for element in words list
+# write every 5 letter word in word list into file
+# close text file
+# return words
+
+import sys
+
+
+def five_letter_word():
+    """
+    function to filter 5 letter words from word_list and return those words.
+    :return words:
+    """
+
+    string = open("word_list.txt", "r")   # opens word list file to read
+    words = [word for word in string.read().split() if len(word) == 5]
+    string.close()                       # close file
+    textfile = open("5letterwords.txt", "w")   # opens file to write 5 letter words
+    for element in words:
+        textfile.write(element + "\n")
+    textfile.close()
+    return words
+
